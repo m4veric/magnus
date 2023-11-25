69824cfbd336daf39a36dcae03377ddefaf95aa0
Kacper <kacprowy@onet.pl>
2023-11-25 16:07:10 +0100

    added javascript in footer for current year and added first changes in contact form

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,15 @@
 const navBtn = document.querySelector('.navbar__nav-btn')
 const naviList = document.querySelector('.navbar__nav-list')
+const spanYear = document.querySelector('.footer__copy-year')
 
 const activeMobileNav = () => {
 	naviList.classList.toggle('active')
 	navBtn.classList.toggle('second-active')
 }
 
+const addCurrentYear = () => {
+	const year = new Date().getFullYear()
+	spanYear.innerText = year
+}
+addCurrentYear()
 navBtn.addEventListener('click', activeMobileNav)
